ae6f21c65107f6204193259afe767e7fec023be5
Shamshad Akther <shamshad.akther@tuni.fi>
2022-04-06 16:26:51 +0300

    Issue#6 & 10: Integration of Server A with RabbitMQ and add order for the sandwich

--- backend/server-a/controllers/Order.js
+++ backend/server-a/controllers/Order.js
@@ -5,20 +5,6 @@ var Order = require('../service/OrderService');
 
 module.exports.addOrder = function addOrder (req, res, next) {
   var order = req.swagger.params['order'].value;
-  /*Order.addOrder(order)
-  .then(function (response) {
-    utils.writeJson(res, response);
-    console.log(order)
-    // Let's add the order to a queue
-    // Notice: "rapid-runner-rabbit" is the name of the Docker Compose service
-    // Using only Docker didn't networking didn't work,
-    // unless Docker's bridge network IPs, were used (172.20.0.X).
-    sendTask.addTask("rapid-runner-rabbit", "received-orders", order);
-
-  })
-  .catch(function (response) {
-    utils.writeJson(res, response);
-  });*/
   Order.addOrder(order)
     .then(function (response) {
       utils.writeJson(res, response);
--- backend/server-a/rabbit-utils/sendTask.js
+++ backend/server-a/rabbit-utils/sendTask.js
@@ -5,19 +5,38 @@
 'use strict';
 
 var amqp = require('amqplib');
+let Order = require('../service/OrderService');
 
 module.exports.addTask = function(rabbitHost, queueName, order){
   amqp.connect('amqp://' + rabbitHost)
-  .then(function(c) {
-    c.createConfirmChannel()
-    .then(function(ch) {
-      ch.sendToQueue(queueName, new Buffer.from(JSON.stringify(order)), {},
-      function(err, ok) {
-        if (err !== null)
-        console.warn(new Date(), 'Message nacked!');
-        else
-        console.log(new Date(), 'Message acked');
-      });
+    .then(function(c) {
+        c.createConfirmChannel()
+            .then(function(ch) {
+                //creating a queue if doesn't exists
+                ch.assertQueue(queueName, {durable: true});
+                //send message to the receive order queue
+                ch.sendToQueue(queueName, new Buffer.from(JSON.stringify(order)), {},
+                    function(err, ok) {
+                        console.log("found order from Server A to RabbitMQ");
+                        let order_id = order.id;
+                        let status;
+                        if (err !== null){
+                            console.warn(new Date(), 'Message nacked!');
+                            status = "failed";
+                        }
+                        else {
+                            console.log(new Date(), 'Message acked');
+                            status = "inQueue";
+                        }
+                        Order.updateOrder(order_id, {status: status})
+                            .then(function (response) {
+                                console.log("Order updated: ", response);
+                            }).catch(function (error) {
+                                console.log("Order update failed", error);
+                            });
+                    });
+            });
+    }).catch((error) => {
+        console.log(error,'Promise error');
     });
-  });
 }
