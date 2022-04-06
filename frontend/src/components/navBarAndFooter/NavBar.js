e09e5fe12ffa64743cc90b4efc9346248b16ac17
The Anh Nguyen <anh.t.nguyen@tuni.fi>
2022-04-07 01:19:39 +0300

    refactor

--- frontend/src/components/navBarAndFooter/NavBar.js
+++ frontend/src/components/navBarAndFooter/NavBar.js
@@ -1,5 +1,5 @@
 import React from "react";
-import logo from "../../images/logo.png"
+import logo from "../../images/logo.png";
 import "../../stylesheets/navBarAndFooter/navBar.css";
 
 import { Link } from "react-router-dom";
